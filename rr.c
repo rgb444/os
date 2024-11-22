160618fab344af8fc7f95601ce50ade1841daa36
rgb444 <tejaspokale2004@gmail.com>
2024-11-22 08:13:57 +0530

    Update rr.c

--- rr.c
+++ rr.c
@@ -2,57 +2,32 @@
 
 struct Process {
     int pid;
-    int arrivalTime;
     int burstTime;
     int remainingTime;
+    int arrivalTime;
     int waitingTime;
     int turnaroundTime;
     int completionTime;
 };
 
 void roundRobin(struct Process processes[], int n, int timeQuantum) {
-    int currentTime = 0, completed = 0, queue[n], front = 0, rear = 0;
-    int visited[n];
+    int currentTime = 0, completed = 0;
 
-    for (int i = 0; i < n; i++) {
-        visited[i] = 0;
-        processes[i].remainingTime = processes[i].burstTime;
-    }
-
-    queue[rear++] = 0;
-    visited[0] = 1;
-
-    while (completed < n) {
-        int i = queue[front++];
-        if (front == n) front = 0;
-
-        if (currentTime < processes[i].arrivalTime) {
-            currentTime = processes[i].arrivalTime;
-        }
-
-        if (processes[i].remainingTime > timeQuantum) {
-            processes[i].remainingTime -= timeQuantum;
-            currentTime += timeQuantum;
-        } else {
-            currentTime += processes[i].remainingTime;
-            processes[i].remainingTime = 0;
-            processes[i].completionTime = currentTime;
-            processes[i].turnaroundTime = processes[i].completionTime - processes[i].arrivalTime;
-            processes[i].waitingTime = processes[i].turnaroundTime - processes[i].burstTime;
-            completed++;
-        }
-
-        for (int j = 0; j < n; j++) {
-            if (j != i && !visited[j] && processes[j].arrivalTime <= currentTime && processes[j].remainingTime > 0) {
-                queue[rear++] = j;
-                if (rear == n) rear = 0;
-                visited[j] = 1;
+    while (completed != n) {
+        for (int i = 0; i < n; i++) {
+            if (processes[i].arrivalTime <= currentTime && processes[i].remainingTime > 0) {
+                if (processes[i].remainingTime <= timeQuantum) {
+                    currentTime += processes[i].remainingTime;
+                    processes[i].remainingTime = 0;
+                    processes[i].completionTime = currentTime;
+                    processes[i].turnaroundTime = processes[i].completionTime - processes[i].arrivalTime;
+                    processes[i].waitingTime = processes[i].turnaroundTime - processes[i].burstTime;
+                    completed++;
+                } else {
+                    currentTime += timeQuantum;
+                    processes[i].remainingTime -= timeQuantum;
+                }
             }
-        }
-
-        if (processes[i].remainingTime > 0) {
-            queue[rear++] = i;
-            if (rear == n) rear = 0;
         }
     }
 }
@@ -60,47 +35,39 @@ void roundRobin(struct Process processes[], int n, int timeQuantum) {
 void displayProcesses(struct Process processes[], int n) {
     float totalWaitingTime = 0, totalTurnaroundTime = 0;
 
-    printf("\n+-----+---------+-------+---------+------------+------------+\n");
-    printf("| PID | Arrival | Burst | Waiting | Turnaround | Completion |\n");
-    printf("+-----+---------+-------+---------+------------+------------+\n");
-
+    printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
     for (int i = 0; i < n; i++) {
-        printf("| %-3d | %-7d | %-5d | %-7d | %-10d | %-10d |\n", 
-               processes[i].pid, 
-               processes[i].arrivalTime, 
-               processes[i].burstTime, 
-               processes[i].waitingTime, 
-               processes[i].turnaroundTime, 
-               processes[i].completionTime);
-
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].pid, processes[i].arrivalTime, processes[i].burstTime, processes[i].waitingTime, processes[i].turnaroundTime);
         totalWaitingTime += processes[i].waitingTime;
         totalTurnaroundTime += processes[i].turnaroundTime;
     }
 
-    printf("+-----+---------+-------+---------+------------+------------+\n");
-    printf("Average Waiting Time: %.2f\n", totalWaitingTime / n);
+    printf("\nAverage Waiting Time: %.2f\n", totalWaitingTime / n);
     printf("Average Turnaround Time: %.2f\n", totalTurnaroundTime / n);
 }
 
 int main() {
     int n, timeQuantum;
 
-    printf("Enter the number of processes:");
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
 
     struct Process processes[n];
 
-    for (int i = 0; i < n; i++) {
-        processes[i].pid = i + 1;
-        printf("\nEnter details for Process %d:\n", i + 1);
-        printf("Arrival Time:");
-        scanf("%d", &processes[i].arrivalTime);
-        printf("Burst Time:");
-        scanf("%d", &processes[i].burstTime);
-    }
-
-    printf("\nEnter the Time Quantum:");
+    printf("Enter the time quantum: ");
     scanf("%d", &timeQuantum);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter process %d details:\n", i + 1);
+        printf("  Arrival time: ");
+        scanf("%d", &processes[i].arrivalTime);
+        printf("  Burst time: ");
+        scanf("%d", &processes[i].burstTime);
+        processes[i].pid = i + 1;
+        processes[i].remainingTime = processes[i].burstTime;
+        processes[i].waitingTime = 0;
+        processes[i].turnaroundTime = 0;
+    }
 
     roundRobin(processes, n, timeQuantum);
     displayProcesses(processes, n);
